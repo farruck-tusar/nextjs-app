da8dc67f322b20c7ac0ebd39597fa07b2c563f03
Farruck Ahamed Tusar <frk.tusar@gmail.com>
2024-02-04 23:55:15 +0100

    move randomQuote to component

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import RandomQuote from './randomQuote';
+import RandomQuote from '../components/randomQuote';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
